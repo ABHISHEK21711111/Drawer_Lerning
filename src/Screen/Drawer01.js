b39d84700b15fbc438298fa6e3ca9291e973f286
ABHISHEK21711111 <ac4126351@gmail.com>
2023-09-01 14:59:09 +0530

    commented out drawer for home screen

--- src/Screen/Drawer01.js
+++ src/Screen/Drawer01.js
@@ -12,7 +12,7 @@ const Drawer01 = () => {
     <NavigationContainer>
     <Drawer.Navigator>
 
-    <Drawer.Screen name="Home" component={HomeScreen} />
+    {/* <Drawer.Screen name="Home" component={HomeScreen} /> */}
     <Drawer.Screen name="RecentTask" component={RecentTask} />
    
   
